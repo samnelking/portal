2957461cf9408e0298c1d255a79b21ca6be23a06
samnel <46660033+samnelking@users.noreply.github.com>
2020-01-28 20:05:11 +0800

    Update options.cmake

--- cmake/options.cmake
+++ cmake/options.cmake
@@ -6,7 +6,7 @@ option(BUILD_GAME_SERVER    "Build game server"                     ON)
 option(BUILD_LOGIN_SERVER   "Build login server"                    ON)
 option(BUILD_EXTRACTORS     "Build map/dbc/vmap/mmap extractors"    OFF)
 option(BUILD_SCRIPTDEV      "Build ScriptDev. (OFF Speedup build)"  ON)
-option(BUILD_PLAYERBOT      "Build Playerbot mod"                   OFF)
+option(BUILD_PLAYERBOT      "Build Playerbot mod"                   ON)
 
 # TODO: options that should be checked/created:
 #option(CLI                  "With CLI"                              ON)
@@ -30,19 +30,5 @@ message(STATUS
     BUILD_SCRIPTDEV         Build scriptdev. (Disable it to speedup build in dev mode by not including scripts)
     BUILD_PLAYERBOT         Build Playerbot mod
 
-  To set an option simply type -D<OPTION>=<VALUE> after 'cmake <srcs>'.
-  Also, you can specify the generator with -G. see 'cmake --help' for more details
-  For example:
-    Build server
-    cmake -DCMAKE_INSTALL_PREFIX=../opt/cmangos ..
-
-    Build full server with extractor
-    cmake -DCMAKE_INSTALL_PREFIX=../opt/cmangos -DBUILD_EXTRACTORS=ON ..
-
-    Build only server without scripts and extractors and in debug mode
-    cmake -DCMAKE_INSTALL_PREFIX=../opt/cmangos -DDEBUG=ON -DBUILD_ALL_SCRIPTS=OFF ..
-
-    Build only login server
-    cmake -DCMAKE_INSTALL_PREFIX=../opt/cmangos -DBUILD_GAME_SERVER=OFF .."
 )
 message("")
